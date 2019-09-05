a066d4f10e8885c42c276878cff490ab121542fb
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-09-05 17:04:48 -0300

    centralize lnk-multisite

--- lnk-rest-api-multisite-endpoints.php
+++ lnk-rest-api-multisite-endpoints.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name: LNK-REST-API-MULTISITE Endpoints
+ * Plugin Name: LNK-REST-API-MULTISITE Endpoints para UPCN
  * Plugin URI: https://github.com/linkerx/lnk-rest-api-sites-endpoint
  * Description: Endpoints varios para Wordpress Multisite
  * Version: 0.1
@@ -38,11 +38,17 @@ function lnk_sites_register_route(){
     'methods' => WP_REST_Server::READABLE,
     'callback' => 'lnk_get_site_posts',
   ));
-  
+
   // Endpoint: Ultimos Post de Todos Los Sitios
   register_rest_route( $route, '/sites-posts', array(
     'methods' => WP_REST_Server::READABLE,
     'callback' => 'lnk_get_sites_posts',
+  ));
+
+  // Endpoint: Posts Destacados de Todos Los Sitios
+  register_rest_route( $route, '/sites-featured-posts', array(
+    'methods' => WP_REST_Server::READABLE,
+    'callback' => 'lnk_get_sites_featured_posts',
   ));
 }
 add_action( 'rest_api_init', 'lnk_sites_register_route');
@@ -61,8 +67,15 @@ function lnk_get_sites(WP_REST_Request $request) {
   if(is_array($sites))
   foreach($sites as $key => $site){
     switch_to_blog($site->blog_id);
-    $info = get_bloginfo('name');
-    $sites[$key]->blog_name = $info;
+    $sites[$key]->blog_name = get_bloginfo('name');
+    $sites[$key]->blog_description = get_bloginfo('description');
+    $sites[$key]->wpurl = get_bloginfo('wpurl');
+     // info delegacion
+  $sites[$key]->info_direccion = get_option('delegacion_info_direccion','Calle XXX, Localidad, Río Negro');
+  $sites[$key]->info_telefono = get_option('delegacion_info_telefono','+54 XXX - XXX XXXX');
+  $sites[$key]->info_email = get_option('delegacion_info_email','email_delegacion@upcn-rionegro.com.ar');
+  $sites[$key]->info_imagen = get_option('delegacion_info_imagen','http://back.upcn-rionegro.com.ar/wp-content/uploads/2003/04/logo_upcn.jpg');
+
     restore_current_blog();
   }
   return new WP_REST_Response($sites, 200 );
@@ -86,15 +99,16 @@ function lnk_get_site(WP_REST_Request $request){
   $site = $sites[0];
 
   switch_to_blog($site->blog_id);
-  $site->frontpage = get_option('page_on_front');
-
-  if($site->frontpage != 0){
-    $site->page = get_post($site->frontpage);
-  }
-
   $site->blog_name = get_bloginfo('name');
   $site->blog_description = get_bloginfo('description');
   $site->wpurl = get_bloginfo('wpurl');
+
+  // info delegacion
+  $site->info_direccion = get_option('delegacion_info_direccion','Calle XXX, Localidad, Río Negro');
+  $site->info_telefono = get_option('delegacion_info_telefono','+54 XXX - XXX XXXX');
+  $site->info_email = get_option('delegacion_info_email','email_delegacion@upcn-rionegro.com.ar');
+  $site->info_imagen = get_option('delegacion_info_imagen','http://back.upcn-rionegro.com.ar/wp-content/uploads/2003/04/logo_upcn.jpg');
+
   restore_current_blog();
 
   return new WP_REST_Response($site, 200 );
@@ -141,6 +155,7 @@ function lnk_get_site_posts(WP_REST_Request $request){
   return new WP_REST_Response($posts, 200 );
 }
 
+
 /**
  * Lista de posts de todos los sitios
  *
@@ -161,8 +176,16 @@ function lnk_get_site_posts(WP_REST_Request $request){
      switch_to_blog($site->blog_id);
 
      $posts_args = array(
-        'numberposts' => $count
-     );
+      'numberposts' => $count,
+      'meta_query' => array(
+        'relation' => 'OR',
+        array(
+          'key' => 'lnk_onhome',
+          'compare' => '=',
+          'value' => '1'
+        )
+    )
+   );
      $posts = get_posts($posts_args);
 
      foreach($posts as $post_key => $post){
@@ -185,9 +208,64 @@ function lnk_get_site_posts(WP_REST_Request $request){
      restore_current_blog();
    }
    usort($allPosts,'lnk_compare_by_date');
-   $allPosts = array_slice($allPosts,0,$count);
    return new WP_REST_Response($allPosts, 200 );
  }
+
+/**
+ * Lista de posts destacados de todos los sitios
+ *
+ * @param WP_REST_Request $request
+ * @return WP_REST_Response: Lista de posts
+ */
+function lnk_get_sites_featured_posts(WP_REST_Request $request){
+
+  $count = $request->get_param("count");
+
+  $sites_args = array(
+    'public' => 1 // los posts tb solo publicos?
+  );
+  $sites = get_sites($sites_args);
+  $allPosts = array();
+  if(is_array($sites))
+  foreach($sites as $site_key => $site){
+    switch_to_blog($site->blog_id);
+
+    $posts_args = array(
+     'numberposts' => '-1',
+     'meta_query' => array(
+       array(
+        'key' => 'lnk_featured',
+        'compare' => '=',
+        'value' => '1'
+      )
+   )
+  );
+    $posts = get_posts($posts_args);
+
+    foreach($posts as $post_key => $post){
+      $posts[$post_key]->blog = array(
+        'blog_id' => $site->blog_id,
+        'blog_name' => get_bloginfo('name'),
+        'blog_url' => $site->path
+      );
+
+
+      $terms = wp_get_post_categories($post->ID);
+      if(is_array($terms)){
+        $posts[$post_key]->the_term = get_term($terms[0])->slug;
+      }
+
+      $posts[$post_key]->thumbnail = get_the_post_thumbnail_url($post->ID);
+      $posts[$post_key]->lnk_featured_mode = get_post_meta($post->ID,'lnk_featured_mode',true);
+    }
+
+    $allPosts = array_merge($allPosts,$posts);
+    restore_current_blog();
+  }
+  usort($allPosts,'lnk_compare_by_date');
+  return new WP_REST_Response($allPosts, 200 );
+}
+
 
  /**
   * Compara 2 objetos WP_Post para ordenar decrecientemente
